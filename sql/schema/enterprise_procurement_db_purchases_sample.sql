07ffeb0773979acb03db952cf5e09ef49b945b4e
Kiranmai-Ch85 <kiranmaich58@gmail.com>
2026-07-26 22:51:54 +0530

    Add files via upload

---
+++ sql/schema/enterprise_procurement_db_purchases_sample.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: enterprise_procurement_db
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `purchases_sample`
+--
+
+DROP TABLE IF EXISTS `purchases_sample`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchases_sample` (
+  `InventoryId` varchar(100) DEFAULT NULL,
+  `Store` int DEFAULT NULL,
+  `Brand` int DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Size` varchar(50) DEFAULT NULL,
+  `VendorNumber` int DEFAULT NULL,
+  `VendorName` varchar(255) DEFAULT NULL,
+  `PONumber` int DEFAULT NULL,
+  `PODate` date DEFAULT NULL,
+  `ReceivingDate` date DEFAULT NULL,
+  `InvoiceDate` date DEFAULT NULL,
+  `PayDate` date DEFAULT NULL,
+  `PurchasePrice` double DEFAULT NULL,
+  `Quantity` int DEFAULT NULL,
+  `Dollars` double DEFAULT NULL,
+  `Classification` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-26 22:32:38
---
+++ sql/schema/enterprise_procurement_db_vendor_sales_summary.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: enterprise_procurement_db
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vendor_sales_summary`
+--
+
+DROP TABLE IF EXISTS `vendor_sales_summary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vendor_sales_summary` (
+  `VendorNumber` int DEFAULT NULL,
+  `VendorName` varchar(255) DEFAULT NULL,
+  `Brand` int DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `PurchasePrice` double DEFAULT NULL,
+  `ActualPrice` double DEFAULT NULL,
+  `Volume` int DEFAULT NULL,
+  `TotalPurchaseQuantity` int DEFAULT NULL,
+  `TotalPurchaseDollars` double DEFAULT NULL,
+  `TotalSalesQuantity` int DEFAULT NULL,
+  `TotalSalesPrice` double DEFAULT NULL,
+  `TotalSalesDollars` double DEFAULT NULL,
+  `TotalExciseTax` double DEFAULT NULL,
+  `FreightCost` double DEFAULT NULL,
+  `GrossProfit` double DEFAULT NULL,
+  `ProfitMargin` double DEFAULT NULL,
+  `StockTurnover` int DEFAULT NULL,
+  `SalestoPurchaseRatio` double DEFAULT NULL,
+  KEY `idx_vendor_sales_vendor` (`VendorNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-26 22:32:56
